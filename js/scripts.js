fbe835d03fa1d1b550e18bba4aa8b326b6bc2958
“maxwellkimutai” <maxwellkimutai@gmail.com>
2018-08-10 17:28:27 +0300

    call function

--- js/scripts.js
+++ js/scripts.js
@@ -42,3 +42,6 @@ function triangleTracker() {
         } else if ((sides[0] !== sides[1] && sides[0] !== sides[2]) || (sides[1] !== sides[2] && sides[1] !== sides[0]) || (sides[2] !== sides[1] && sides[2] !== sides[0]) ) {
       alert("The three sides make a scalene triangle!!!");
         }
+}
+
+triangleTracker();
